4b70ceef3579b6a3b597621b0833818c1182caab
Sadat Ali <sa9621952@gmail.com>
2025-06-27 18:00:23 +0530

    Add new files and update existing ones

---
+++ delta_react/basic-react/src/Title.jsx
@@ -0,0 +1,11 @@
+import "./Title.css"
+function Title() {
+  return (
+    <div className="title">
+        <h1>I am the Title</h1>
+        <h3>I am the Description</h3>
+    </div>
+  );
+}
+  
+export default Title;
---
+++ delta_react/basic-react/src/TitleTab.jsx
@@ -0,0 +1,12 @@
+import Title from "./Title.jsx";
+
+function TitleTab() {
+  return (
+    <>
+      <Title />
+     <Title />
+    </>
+  );
+}
+
+export default TitleTab;
